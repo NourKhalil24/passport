fdc33cbfb7d7715ab620b3f8ebcf551334dda516
NourKhalil24 <nk146396@gmail.com>
2024-03-20 00:30:38 +0300

    first commit

---
+++ app/Http/Controllers/Auth/LoginRegisterController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Validator;
+
+class LoginRegisterController extends Controller
+{
+     /**
+     * Register a new user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+        
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $data['token'] = $user->createToken($request->email)->accessToken;
+        $data['user'] = $user;
+
+        $response = [
+            'status' => 'success',
+            'message' => 'User is created successfully.',
+            'data' => $data,
+        ];
+
+        return response()->json($response, 201);
+    }
+
+    /**
+     * Authenticate the user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        
+        // Check email exist
+        $user = User::where('email', $request->email)->first();
+
+        // Check password
+        if(!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Invalid credentials'
+                ], 401);
+        }
+
+        $data['token'] = $user->createToken($request->email)->accessToken;
+        $data['user'] = $user;
+        
+        $response = [
+            'status' => 'success',
+            'message' => 'User is logged in successfully.',
+            'data' => $data,
+        ];
+
+        return response()->json($response, 200);
+    } 
+
+    /**
+     * Log out the user from application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User is logged out successfully'
+            ], 200);
+    }    
+}
